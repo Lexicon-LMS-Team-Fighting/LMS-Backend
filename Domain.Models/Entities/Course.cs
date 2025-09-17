b125e33ce93409c6c3b1eefe9446e30836930a4e
Bosseyang <bosse_yang@hotmail.com>
2025-09-17 15:53:54 +0200

    Create course entity with its relations, the relations are currently commented to keep the code from errors. Uncomment them as they get added in the future.

---
+++ Domain.Models/Entities/Course.cs
@@ -0,0 +1,27 @@
+﻿namespace Domain.Models.Entities
+
+    /// <summary>
+    /// Represents the relationship between a course and module, course and document.
+    /// 
+    /// This entity has the following relations: 1:m with Module, 1:m with Document.
+    /// </summary>
+{
+    public class Course
+    {
+        public Guid CourseId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        // Relations
+        // TODO: Add relations
+        //public ICollection<Module> Modules { get; set; } = new List<Module>();
+        //public ICollection<Document>? Documents { get; set; } = new List<Document>();
+        //public ICollection<UserCourse> UserCourses { get; set; } = new List<UserCourse>();
+    }
+}
