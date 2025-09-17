af157d6b12632ab36f8bc1c4efd2768ac67b30c5
a21jeaha <hisui86@gmail.com>
2025-09-17 15:40:14 +0200

    Created UserCourse
    Foreign key element is commented to keep code in a runnable state.
    It should be un-commented when relations between entities are established.

---
+++ Domain.Models/Entities/UserCourse.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models.Entities;
+
+/// <summary>
+/// Represents the relationship between a user and a course.
+/// This entity has a many-to-one relationship between <c>User</c> and <c>Course</c>.
+/// </summary>
+public class UserCourse
+{
+	public Guid UserId { get; set; }
+	public Guid CourseId { get; set; }
+	//public User User { get; set; }
+}									
