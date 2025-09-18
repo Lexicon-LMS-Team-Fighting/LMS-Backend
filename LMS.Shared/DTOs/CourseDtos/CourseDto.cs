4919ed05866fcb4639f864c9fb30799fb32e1aa0
Bosseyang <bosse_yang@hotmail.com>
2025-09-18 16:03:48 +0200

    Create CourseDto.cs
    Create CourseDto with it's relevant fields .

---
+++ LMS.Shared/DTOs/CourseDtos/CourseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Shared.DTOs.CourseDtos;
+public class CourseDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+}
