d17496ca9fe227f4fd0f135bcdb57635f742b51c
Bosseyang <bosse_yang@hotmail.com>
2025-09-19 10:05:41 +0200

    Update CourseDto.cs
    Remove validation attributes

--- LMS.Shared/DTOs/CourseDtos/CourseDto.cs
+++ LMS.Shared/DTOs/CourseDtos/CourseDto.cs
@@ -10,13 +10,9 @@ namespace LMS.Shared.DTOs.CourseDtos;
 public class CourseDto
 {
     public Guid Id { get; set; }
-    [Required]
-    [MaxLength(255, ErrorMessage = "Max length is 255.")]
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    [Required]
     public DateTime StartDate { get; set; }
-    [Required]
     public DateTime EndDate { get; set; }
 
     //Relations
