6f51421387252a02e8f324606a8df174c29e83d4
Alexander <alexander@MacBook-Pro-Albina.local>
2025-09-19 11:17:14 +0200

    Add missing ApplicationUser fields

--- Domain.Models/Entities/ApplicationUser.cs
+++ Domain.Models/Entities/ApplicationUser.cs
@@ -8,6 +8,8 @@ namespace Domain.Models.Entities;
 /// </summary>
 public class ApplicationUser : IdentityUser
 {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public string? RefreshToken { get; set; }
     public DateTime RefreshTokenExpireTime { get; set; }
 
