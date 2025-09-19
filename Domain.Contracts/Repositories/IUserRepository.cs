aa9d3741ac533e870b9aa0f5d87faa64e0ecfd12
a21jeaha <hisui86@gmail.com>
2025-09-19 10:48:56 +0200

    Created I & UserRepository not tested yet

---
+++ Domain.Contracts/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models.Entities;
+
+namespace Domain.Contracts.Repositories;
+
+public interface IUserRepository: IRepositoryBase<User>
+{
+	Task<User?> GetUserAsync(Guid userId, bool changeTracking = false);
+	Task<List<User>> GetUsersAsync(bool changeTracking = false);
+}
---
+++ LMS.Infractructure/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
+using LMS.Infractructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Infractructure.Repositories;
+
+public class UserRepository : RepositoryBase<User>, IUserRepository
+{
+	public UserRepository(ApplicationDbContext context): base(context)
+	{}
+
+	// Not used yet. Parameters set to change.
+	void IRepositoryBase<User>.Create(User entity)
+	{
+		throw new NotImplementedException();
+	}
+
+	// Not used yet. Parameters set to change.
+	void IRepositoryBase<User>.Delete(User entity)
+	{
+		throw new NotImplementedException();
+	}
+
+	async Task<User?> IUserRepository.GetUserAsync(Guid userId, bool changeTracking) => 
+		await FindByCondition(u => u.Id.Equals(userId), changeTracking)
+				.FirstOrDefaultAsync();
+
+
+	async Task<List<User>> IUserRepository.GetUsersAsync(bool changeTracking) => 
+		await FindAll(changeTracking).ToListAsync();
+	
+
+	// Not used yet. Parameters set to change.
+	void IRepositoryBase<User>.Update(User entity)
+	{
+		throw new NotImplementedException();
+	}
+}
