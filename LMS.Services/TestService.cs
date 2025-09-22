4984770a7d4cbf315ecbcd992a536c86727583fa
a21jeaha <hisui86@gmail.com>
2025-09-22 09:55:11 +0200

    Added a sequence of connections between Controller -> Service -> Repository to ease testing. for

---
+++ LMS.Services/TestService.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Contracts.Repositories;
+using Service.Contracts;
+using System.Threading.Tasks;
+
+namespace LMS.Services;
+
+// ToDo: this class is here for testing purposes only, should be removed when no longer needed.
+public class TestService: ITestService
+{
+	public IUnitOfWork unit;
+
+	public TestService(IUnitOfWork unitOfWork)
+	{
+		unit = unitOfWork;
+		
+	}
+
+	public async void TestMethod()
+	{
+		var result = await unit.User.GetUsersAsync();
+		Console.WriteLine("test");
+	}
+}
