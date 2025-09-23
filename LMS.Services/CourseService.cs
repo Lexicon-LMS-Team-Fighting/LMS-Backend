c97b80fb5a1e3a2772e7de17ce2757e008d66cb0
a21jeaha <hisui86@gmail.com>
2025-09-23 11:46:43 +0200

    Created Interface and Class for CourseService and mapping between Course -> CourseDto

---
+++ LMS.Services/CourseService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Domain.Contracts.Repositories;
+using Domain.Models.Exceptions;
+using LMS.Shared.DTOs.CourseDtos;
+using Service.Contracts;
+
+namespace LMS.Services;
+
+/// <summary>
+/// Provides operations related to courses, including retrieval of single or multiple courses.
+/// Implements the <see cref="ICourseService"/> interface.
+/// </summary>
+public class CourseService : ICourseService
+{
+	private readonly IUnitOfWork _unitOfWork;
+	private readonly IMapper _mapper;
+
+	public CourseService(IUnitOfWork unitOfWork, IMapper mapper)
+	{
+		_unitOfWork = unitOfWork;
+		_mapper = mapper;
+	}
+
+	public async Task<CourseDto> GetCourseAsync(Guid courseId)
+	{
+		var course = await _unitOfWork.Course.GetCourseAsync(courseId);
+		
+		if (course is null) 
+			throw new NotFoundException($"Course with id: {courseId} was not found.");
+
+		return _mapper.Map<CourseDto>(course);
+	}
+
+	public async Task<IEnumerable<CourseDto>> GetCoursesAsync()
+	{
+		var courses = await _unitOfWork.Course.GetCoursesAsync();
+
+		return _mapper.Map<IEnumerable<CourseDto>>(courses);
+	}
+}
---
+++ Service.Contracts/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using LMS.Shared.DTOs.CourseDtos;
+
+namespace Service.Contracts;
+
+/// <summary>
+/// Defines the contract for course-related business logic operations. <br/>
+/// Provides methods to retrieve user data as <see cref="CourseDto"/> objects.
+/// </summary>
+public interface ICourseService
+{
+	Task<CourseDto> GetCourseAsync(Guid courseId);
+	Task<IEnumerable<CourseDto>> GetCoursesAsync();
+}
