51013486085a3166adb90a848c1984f034b4cce1
aleksandr-papajanov <aleksandr.papajanov@gmail.com>
2025-10-06 10:08:01 +0200

    Create feedback controller

---
+++ LMS.Presentation/Controllers/FeedbackController.cs
@@ -0,0 +1,136 @@
+﻿using LMS.Shared.DTOs.LMSActivityDtos;
+using LMS.Shared.DTOs.LMSActivityFeedbackDtos;
+using LMS.Shared.DTOs.ModuleDtos;
+using LMS.Shared.DTOs.PaginationDtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace LMS.Presentation.Controllers
+{
+    /// <summary>
+    /// Controller for managing feedbacks.
+    /// Provides endpoints for common CRUD operations on feedback entities.
+    /// </summary>
+    [Route("api/feedbacks")]
+    [ApiController]
+    [Authorize]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeedbackController"/> class.
+        /// </summary>
+        /// <param name="serviceManager">The service manager for accessing module-related services.</param>
+        public FeedbackController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        /// <summary>
+        /// Retrieves a specific feedback by its unique identifier.
+        /// </summary>
+        /// <param name="guid">The unique identifier of the feedback.</param>
+        /// <param name="include">Related entities to include (e.g., "lmsactivities", "participants", "documents").</param>
+        /// <returns>A <see cref="LMSActivityFeedbackExtendedDto"/> representing the feedback.</returns>
+        /// <response code="200">Returns the feedback details.</response>
+        /// <response code="404">If no feedback is found with the specified GUID.</response>
+        /// <response code="401">Unauthorized.</response>
+        /// <response code="403">Forbidden.</response>
+        [HttpGet("{guid}")]
+        [Authorize(Roles = "Teacher,Student")]
+        [SwaggerOperation(
+            Summary = "Get specified feedback by ID",
+            Description = "Retrieves feedback details by their unique GUID identifier."
+        )]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LMSActivityFeedbackExtendedDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<LMSActivityFeedbackExtendedDto>> GetFeedback(Guid guid, [FromQuery] string? include) =>
+            Ok(await _serviceManager.FeedbackService.GetByIdAsync(guid, include));
+
+        /// <summary>
+        /// Creates a new feedback.
+        /// </summary>
+        /// <param name="module">The details of the feedback to create.</param>
+        /// <returns>The created feedback.</returns>
+        /// <response code="201">Returns the created feedback.</response>
+        /// <response code="400">If the provided feedback data is invalid.</response>
+        /// <response code="409">If a feedback with the same identifier already exists.</response>
+        /// <response code="401">Unauthorized.</response>
+        /// <response code="403">Forbidden.</response>
+        [HttpPost]
+        [Authorize(Roles = "Teacher")]
+        [SwaggerOperation(
+            Summary = "Create a new feedback",
+            Description = "Creates a new feedback with the provided details."
+        )]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(LMSActivityFeedbackExtendedDto))]
+        [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult<LMSActivityFeedbackExtendedDto>> CreateFeedback([FromBody] CreateModuleDto module)
+        {
+            var createdModule = await _serviceManager.FeedbackService.CreateAsync(module);
+            return CreatedAtAction(nameof(GetFeedback), new { guid = createdModule.Id }, createdModule);
+        }
+
+        /// <summary>
+        /// Updates an existing feedback.
+        /// </summary>
+        /// <param name="guid">The unique identifier of the feedback to update.</param>
+        /// <param name="module">The updated details of the feedback.</param>
+        /// <response code="204">Feedback was successfully updated.</response>
+        /// <response code="400">If the provided feedback data is invalid.</response>
+        /// <response code="404">If no feedback is found with the specified GUID.</response>
+        /// <response code="401">Unauthorized.</response>
+        /// <response code="403">Forbidden.</response>
+        /// <response code="409">If there is a conflict while updating the feedback.</response>
+        [HttpPut("{guid}")]
+        [Authorize(Roles = "Teacher")]
+        [SwaggerOperation(
+            Summary = "Update an existing feedback",
+            Description = "Updates the details of an existing feedback identified by its GUID."
+        )]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(ProblemDetails))]
+        public async Task<IActionResult> UpdateFeedback(Guid guid, [FromBody] UpdateModuleDto module)
+        {
+            await _serviceManager.FeedbackService.UpdateAsync(guid, module);
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Deletes a feedback by its unique identifier.
+        /// </summary>
+        /// <param name="guid">The unique identifier of the feedback to delete.</param>
+        /// <response code="204">Feedback was successfully deleted.</response>
+        /// <response code="404">If no feedback is found with the specified GUID.</response>
+        /// <response code="401">Unauthorized.</response>
+        /// <response code="403">Forbidden.</response>
+        [HttpDelete("{guid}")]
+        [Authorize(Roles = "Teacher")]
+        [SwaggerOperation(
+            Summary = "Delete a feedback",
+            Description = "Deletes the feedback identified by its GUID."
+        )]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> DeleteFeedback(Guid guid)
+        {
+            await _serviceManager.FeedbackService.DeleteAsync(guid);
+            return NoContent();
+        }
+    }
+}
